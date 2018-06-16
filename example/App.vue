5cf439af8677548763721b965c4416d61ecf3cc7
jiangshanmeta <540118044@qq.com>
2018-06-16 11:28:21 +0800

    init project and example

---
+++ example/App.vue
@@ -0,0 +1,26 @@
+<template>
+	<div id="app">
+    <h3>Template code: </h3>
+    <code v-html="template"></code>
+    <br><br><br><br>
+    <h3>As you can see, the v-runtime-template component has access to this component scope, and updates accordingly. </h3>    
+    <input v-model="name"/> {{name}}
+		<!-- <v-runtime-template :template="template"></v-runtime-template> -->
+	</div>
+</template>
+
+<script>
+import AppMessage from "./AppMessage";
+
+export default {
+  data: () => ({
+    name: "Mellow",
+    template: `
+      <app-message>Hello {{ name }}!</app-message>
+    `
+  }),
+  components: {
+    AppMessage,
+  }
+}
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+function resolve (dir) {
+  return path.join(__dirname,  dir)
+}
+
+const examplePath = resolve('example');
+const srcPath = resolve('src');
+
+
+module.exports = {
+    entry:{
+        main:"./example/main.js",
+    },
+    output:{
+        filename:'[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.js', '.vue', '.json'],
+        alias: {
+            '@': examplePath,
+            'src': srcPath,
+        },
+    },
+    devServer:{
+        contentBase:"./dist"
+    },
+    module:{
+        rules:[
+            {
+                test:/\.js$/,
+                loader:"babel-loader",
+                include:[srcPath,examplePath]
+            },
+            {
+                test:/\.vue$/,
+                loader:"vue-loader",
+                include:[srcPath,examplePath],
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+
+        ]
+    },
+    plugins:[
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+            inject: true
+        }),
+
+    ],
+}
